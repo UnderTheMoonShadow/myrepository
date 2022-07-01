b7f983692a26e523c3cba9a2251180347d6791a7
root <root>
2022-07-01 18:58:37 +0800

    添加了一个test方法

--- house/src/main/java/com/x/utils/Test.java
+++ house/src/main/java/com/x/utils/Test.java
@@ -4,4 +4,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("这是一个测试类");
     }
+
+    public void test() {
+        System.out.println("添加test方法");
+    }
 }
